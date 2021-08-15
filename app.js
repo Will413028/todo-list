e0739d81dd4bb9803d7b63e70fa887803b8e059c
william1993314 <william1993314@gmail.com>
2021-08-15 08:14:25 -0400

    Add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
 
 const app = express()
 
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () =>{
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected')
+})
 app.get('/', (req, res) => {
     res.send('hello world!')
 })
